283093baa475215d5e2f8ed2a86e46da1e90eee1
QuentinDup22 <quentin.dupuy69@gmail.com>
2021-01-15 17:21:30 +0100

    Added all stuff...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Plugins } from '@capacitor/core';
+
+const { Geolocation,Network } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http:HttpClient) {}
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+  }
+
+  async getNetwork(){
+    const status = await Network.getStatus();
+    console.log('Network Status ', status);
+  }
+
+
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+
+    this.getCurrentPosition();
+    this.getNetwork();
+
+    const URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
+
+    this.http.get(URL).subscribe ((data) => {
+      console.log(data);
+    })
+    
+  }
 
 }
